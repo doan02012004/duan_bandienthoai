8f091a53b7c45adf2e8a326966157072328a980a
Giangdz2004 <giangnxph32259@fpt.edu.vn>
2023-11-16 15:17:12 +0700

    Update add.php

--- view/admin/baiviet/add.php
+++ view/admin/baiviet/add.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
     <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Thêm bài viết</h3>
+                <h3 class="card-title">Thêm bài viếtt</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
