a9dfebe610e103f67939380549cd1835e4f2387c
doan02012004 <doana1k24@gmail.com>
2023-11-16 08:00:56 +0700

    sửa hệ thống

--- view/admin/hethong/add.php
+++ view/admin/hethong/add.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
     <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Thêm Banner/Logo</h3>
+                <h3 class="card-title">Thêm hệ thống</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
--- view/admin/hethong/update.php
+++ view/admin/hethong/update.php
@@ -1,18 +1,18 @@
 <div class="content-wrapper">
     <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Cập nhật Banner/Logo</h3>
+                <h3 class="card-title">Cập nhật hệ thống</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form>
                 <div class="card-body">
                   <div class="form-group">
-                    <label for="exampleInputEmail1">Tên</label>
+                    <label for="exampleInputEmail1">Tên cửa hàng</label>
                     <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nhập tên danh mục">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputFile">Logo/Banner</label>
+                    <label for="exampleInputFile">Logo</label>
                     <div class="input-group">
                       <div class="custom-file">
                         <input type="file" class="custom-file-input" id="exampleInputFile">
@@ -24,12 +24,35 @@
                     </div>
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Trạng thái</label>
+                    <label for="exampleInputPassword1">Số điện thoại</label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder=" Nhập trạng thái danh mục">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Mô tả</label>
+                    <label for="exampleInputPassword1">Email</label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nhập mô tả">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Địa chỉ</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nhập mô tả">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Trạng thái</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nhập mô tả">
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <!-- select -->
+                      <div class="form-group">
+                        <label>Hãng sản phẩm</label>
+                        <select class="form-control">
+                          <option>option 1</option>
+                          <option>option 2</option>
+                          <option>option 3</option>
+                          <option>option 4</option>
+                          <option>option 5</option>
+                        </select>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <!-- /.card-body -->
