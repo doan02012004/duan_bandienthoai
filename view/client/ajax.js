ab142825479c9bc862726fc8a3e662744e2058bf
doan02012004 <doana1k24@gmail.com>
2023-11-24 17:10:13 +0700

    update ajax

--- view/client/ajax.js
+++ view/client/ajax.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
         $('.locgia').change(function(){
             var locgia = $('.locgia').val();
-            alert(locgia);
             $.post("view/model/locgia.php",{locgia : locgia},function(data){
                 $('.sanpham').html(data);
             });
